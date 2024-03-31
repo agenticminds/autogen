84229ce9b63844143d3564e9b67f2a3887264074
Li Jiang <bnujli@gmail.com>
2024-03-31 21:50:32 +0800

    Init RetrieverAgent

---
+++ autogen/agentchat/contrib/retriever_agent.py
@@ -0,0 +1,7 @@
+from autogen.agentchat import ConversableAgent
+
+
+class RetrieverAgent(ConversableAgent):
+    """Retriever Agent is a subclass of ConversableAgent which can retrieve documents based on its received message."""
+
+    pass
